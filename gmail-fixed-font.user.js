b46d5af00359232087b4a0e1b0934ef213611d5c
Jon Parise <jon@indelible.org>
2009-07-13 23:27:02 -0700

    Initial import of version 1.22.

---
+++ gmail-fixed-font.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name           Gmail Fixed Font
+// @namespace      http://www.indelible.org/
+// @description    Fixed-font message bodies for Gmail
+// @author         Jon Parise
+// @version        1.21
+// @include        http://mail.google.com/*
+// @include        https://mail.google.com/*
+// @include        http://*.mail.google.com/*
+// @include        https://*.mail.google.com/*
+// ==/UserScript==
+
+// Plain-text Message Body
+var css = ".ii { font: .9em monospace !important; }";
+// Quoted Text
+css += ".im, .gmail_quote { color: #666 !important; }";
+
+if (typeof GM_addStyle != "undefined") {
+    GM_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+    addStyle(css);
+} else {
+    var heads = document.getElementsByTagName("head");
+    if (heads.length > 0) {
+        var node = document.createElement("style");
+        node.type = "text/css";
+        node.appendChild(document.createTextNode(css));
+        heads[0].appendChild(node);
+    }
+}
